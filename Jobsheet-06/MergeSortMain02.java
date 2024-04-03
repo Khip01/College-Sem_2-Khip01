00f5dad8ef37235dac78b04a3a0b1106f270ff9f
Khip01 <aakhifathallah24@gmail.com>
2024-04-03 15:56:48 +0700

    add: practicum 6.4

---
+++ Jobsheet-06/MergeSortMain02.java
@@ -0,0 +1,14 @@
+public class MergeSortMain02 {
+    
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("sorting dengan merge sort");
+        MergeSorting02 mSort = new MergeSorting02();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("setelah diurutkan");
+        mSort.printArray(data);
+    }
+
+}
