0e1b6e4e06c2244051851caaf3b01a2bceb5273a
Khip01 <aakhifathallah24@gmail.com>
2024-04-03 15:26:00 +0700

    update: practicum 6.3 due to question

--- Jobsheet-06/PencarianBuku02.java
+++ Jobsheet-06/PencarianBuku02.java
@@ -64,7 +64,7 @@ public class PencarianBuku02 {
             mid = left + (right - left) / 2;
             if (cari == listBk[mid].kodeBuku) {
                 return (mid);
-            } else if (listBk[mid].kodeBuku > cari) {
+            } else if (listBk[mid].kodeBuku < cari) {
                 return FindBinarySearch(cari, left, mid - 1);
             } else {
                 return FindBinarySearch(cari, mid + 1, right);
