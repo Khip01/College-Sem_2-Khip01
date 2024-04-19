4aebb8005d6312c124b3c9b07541cd77ee53a4dd
Khip01 <aakhifathallah24@gmail.com>
2024-04-19 15:07:50 +0700

    update: practicum 2.1 due to question

--- Jobsheet-07/Utama02.java
+++ Jobsheet-07/Utama02.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Utama02 {
 
     public static void main(String[] args) {
-        Gudang02 gudang = new Gudang02(7);
-
         Scanner scanner = new Scanner(System.in);
+
+        // User menentukan dapat kapasitas stack
+        System.out.print("\nMasukkan jumlah kapasitas dari stack: ");
+        int kapasitas = scanner.nextInt();
+
+        Gudang02 gudang = new Gudang02(kapasitas);
 
 
         while (true) {
@@ -13,7 +17,8 @@ public class Utama02 {
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Tampilkan barang teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih operasi: ");
             int pilihan = scanner.nextInt();
             scanner.nextLine();
@@ -37,6 +42,9 @@ public class Utama02 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
                     break;
                 default:
                 System.out.println("Pilihan tidak valid. Silahkan coba lagi.");
